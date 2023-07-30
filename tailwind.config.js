03f739be700a10fde5998a796939df8430817a12
HoqueShaon <cse225jvn@gmail.com>
2023-07-30 16:22:09 +0600

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,26 @@ module.exports = {
     },
     plugins: [],
   }
+//   <div class="">
+//                 <h2 class="text-7xl my-5 font-extrabold text-center">Exotic tastes at your <br /> doorstep</h2>
+//                 <p class="text-xl my-6 mt-4 font-light text-center">We supply highly quality organic products</p>
+//                 <div class="mt-4 ml-20  mx-auto ">
+//                     <button class="btn btn-error w-3/12 text-white items-center ml-20 mx-auto">Shop Now</button>
+//                 </div>
+//             </div>
+
+
+{/* <div id="slide3" class="carousel-item relative w-full max-xl:">
+            <img src="/images/banner3.png" class="w-full brightness-50" />
+            <div class="absolute flex justify-between items-center transform -translate-y-1/2 left-5 right-5 top-1/2  text-white">
+                <a href="slide2" class="btn btn-circle  text-white bg-black border-black">❮</a>
+                <div class="block">
+                    <h2 class="text-7xl my-5 font-extrabold text-center">Exotic tastes at your <br /> doorstep</h2>
+                    <p class="text-xl my-6 mt-4 font-light text-center">We supply highly quality organic products</p>
+                    <div class="mt-4 ml-20  mx-auto ">
+                        <button class="btn btn-error w-3/12 text-white items-center ml-20 mx-auto">Shop Now</button>
+                    </div>
+                </div>
+                <a href="slide4" class="btn btn-circle  text-white bg-black border-black">❯</a>
+            </div>
+        </div> */}
